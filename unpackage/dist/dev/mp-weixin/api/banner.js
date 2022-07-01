2ecb61321e0b913da80d78067834e2acdba4b25d
华晨 <774031882@qq.com>
2022-07-01 10:18:56 +0800

    update

--- unpackage/dist/dev/mp-weixin/api/banner.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var utils_request = require("../utils/request.js");
-function getBanners(data = {}) {
-  return utils_request.request({
-    method: "GET",
-    url: "/banners"
-  });
-}
-exports.getBanners = getBanners;
